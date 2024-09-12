932965e4db5b2eb5804c4e9962f4190611bcfe64
michrag <31441542+michrag@users.noreply.github.com>
2024-09-12 17:02:48 +0200

    Working with Environment Variables

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,26 +1,43 @@
 use std::error::Error;
 use std::fs;
+use std::env;
+
+
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
-            if args.len() < 3 {
-                return Err("not enough arguments");
-            }
-            let query = args[1].clone();
-            let file_path = args[2].clone();
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-            Ok(Config { query, file_path })
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
